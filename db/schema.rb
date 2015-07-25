c975a2d3ef45c8412b1fe284236d59c236953486
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-07-25 15:52:58 -0500

    Updated schema after new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725155715) do
+ActiveRecord::Schema.define(version: 20150725203535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "purchased",  default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
