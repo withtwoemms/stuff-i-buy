324f7a65f63eacf39d635a2025cc7c8f80c969ed
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-07-25 11:22:29 -0500

    Generate ListController

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,22 @@
+class ListController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
