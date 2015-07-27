979c115ff9319830661cdc02d0fed505786ecb1d
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-07-26 19:07:49 -0500

    resolve merge conflict

--- db/migrate/20150725203535_create_items.rb
+++ db/migrate/20150725203535_create_items.rb
@@ -2,11 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string      :name
-<<<<<<< Updated upstream
-      t.boolean     :purchased, default: true
-=======
       t.boolean     :purchased, default: false
->>>>>>> Stashed changes
       t.timestamps
     end
   end
